6655473787688b3f1dea28cfc9287da56d658e49
Sarthak950 <panditsamsar950@gmail.com>
2024-03-17 10:02:37 +0530

    done

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
 const cardList = document.getElementsByClassName("catCard");
 const scrollCon = document.getElementById("catCardCon");
 import gsap from "gsap";
+
+
+let isFolded = false
 // add inactiveCard to the whole list
 for (let i = 0; i < cardList.length; i++) {
     cardList[i].classList.add("inactiveCard");
+    cardList[i].addEventListener("click", ()=> {
+        if (!isFolded) {
+            cardList[0].classList.remove("activeCard");
+            cardList[0].classList.add("inactiveCard");
+        }
+    })
 }
+cardList[0].classList.remove("inactiveCard");
+cardList[0].classList.add("activeCard");
+
 
 let i = 0;
+
+
 // there are 11 cards in the list after 2 sec remove the prev one and then jump to the next one
-setInterval(() => {
-    cardList[i].classList.remove("activeCard");
-    cardList[i].classList.add("inactiveCard");
-    i = (i + 1) % cardList.length;
-
-    if (window.innerWidth < 900) {
-        gsap.to(scrollCon, {
-            x: `-${i * 5}rem`,
-            duration: 1,
-            ease: "power2.inOut",
-        })
-    }
-    cardList[i].classList.remove("inactiveCard");
-    cardList[i].classList.add("activeCard");
-
-    // transfrom the scrollCon to the next card
-    // scrollCon.style.transform = `translateX(-${5 * 16}px)`;
-}, 4000);
-
+// setInterval(() => {
+//     cardList[i].classList.remove("activeCard");
+//     cardList[i].classList.add("inactiveCard");
+//     i = (i + 1) % cardList.length;
+//
+//     if (window.innerWidth < 900) {
+//         gsap.to(scrollCon, {
+//             x: `-${i * 5}rem`,
+//             duration: 1,
+//             ease: "power2.inOut",
+//         })
+//     }
+//     cardList[i].classList.remove("inactiveCard");
+//     cardList[i].classList.add("activeCard");
+//
+//     // transfrom the scrollCon to the next card
+//     // scrollCon.style.transform = `translateX(-${5 * 16}px)`;
+// }, 4000);
+//
 // if window size exceed teh 900px then remove the transform
 window.addEventListener("resize", () => {
     if (window.innerWidth > 900) {
