0bd035eb87a3cb56b616caa4946b9d058ece76d8
Sarthak950 <panditsamsar950@gmail.com>
2024-03-18 17:20:28 +0530

    blank

--- main.js
+++ main.js
@@ -21,27 +21,6 @@ cardList[0].classList.add("activeCard");
 let i = 0;
 
 
-// there are 11 cards in the list after 2 sec remove the prev one and then jump to the next one
-// setInterval(() => {
-//     cardList[i].classList.remove("activeCard");
-//     cardList[i].classList.add("inactiveCard");
-//     i = (i + 1) % cardList.length;
-//
-//     if (window.innerWidth < 900) {
-//         gsap.to(scrollCon, {
-//             x: `-${i * 5}rem`,
-//             duration: 1,
-//             ease: "power2.inOut",
-//         })
-//     }
-//     cardList[i].classList.remove("inactiveCard");
-//     cardList[i].classList.add("activeCard");
-//
-//     // transfrom the scrollCon to the next card
-//     // scrollCon.style.transform = `translateX(-${5 * 16}px)`;
-// }, 4000);
-//
-// if window size exceed teh 900px then remove the transform
 window.addEventListener("resize", () => {
     if (window.innerWidth > 900) {
         gsap.to(scrollCon, {
